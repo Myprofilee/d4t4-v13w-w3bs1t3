ff9998aff2d1f30c73a21a498867ed0aff47c807
Myprofilee <andrep13.id@gmail.com>
2023-04-23 14:35:18 +0700

    first commit

--- firebase/andrep/home/andrep.js
+++ firebase/andrep/home/andrep.js
@@ -1,39 +1,39 @@
 var config = {
-    apiKey: "AIzaSyCEliugQ19EDDcFKQnJCK7xUWtqBEWCnQo",
-    authDomain: "myzuu-view.firebaseapp.com",
-    databaseURL: "https://myzuu-view-default-rtdb.firebaseio.com",
-    projectId: "myzuu-view",
-    storageBucket: "myzuu-view.appspot.com",
-    messagingSenderId: "79139842519",
-    appId: "1:79139842519:web:0a77efc456a6fc9bf50392",
-    measurementId: "G-5P5Y0N9T9Y"
-  };
-  
-  firebase.initializeApp(config);
-  
-  // Create a variable to reference the database.
-  var database = firebase.database();
-  
-  var connectionsRef = database.ref("/view aplikasi");
-  var connectedRef = database.ref(".info/connected");
-  
-  // When the client's connection state changes...
-  connectedRef.on("value", function (snap) {
-  
+    apiKey: "AIzaSyAYx32jmuvgmRHmY795BxpdSfq1AYC4Z4I",
+    authDomain: "andrep-id.firebaseapp.com",
+    databaseURL: "https://andrep-id-default-rtdb.firebaseio.com",
+    projectId: "andrep-id",
+    storageBucket: "andrep-id.appspot.com",
+    messagingSenderId: "901866141368",
+    appId: "1:901866141368:web:0beedf714b54d13e8e264d",
+    measurementId: "G-TFKQP4C41V"
+};
+
+firebase.initializeApp(config);
+
+// Create a variable to reference the database.
+var database = firebase.database();
+
+var connectionsRef = database.ref("/andrep Home");
+var connectedRef = database.ref(".info/connected");
+
+// When the client's connection state changes...
+connectedRef.on("value", function (snap) {
+
     // If they are connected..
     if (snap.val()) {
-  
-      // Add user to the connections list.
-      var con = connectionsRef.push(true);
-      // Remove user from the connection list when they disconnect.
-      con.onDisconnect().remove();
+
+        // Add user to the connections list.
+        var con = connectionsRef.push(true);
+        // Remove user from the connection list when they disconnect.
+        con.onDisconnect().remove();
     }
-  });
-  
-  // When first loaded or when the connections list changes...
-  connectionsRef.on("value", function (snap) {
-  
+});
+
+// When first loaded or when the connections list changes...
+connectionsRef.on("value", function (snap) {
+
     // Display the viewer count in the html.
     // The number of online users is the number of children in the connections list.
-    $("#watchers-home").text(snap.numChildren());
-  });
+    $("#andrep-home").text(snap.numChildren());
+});

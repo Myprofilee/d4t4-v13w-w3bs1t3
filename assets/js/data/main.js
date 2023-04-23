cacea61f861989b4cb0f5e5bc642b819f611c628
Myprofilee <andrep13.id@gmail.com>
2023-04-23 22:42:57 +0700

    first commit

--- assets/js/data/main.js
+++ assets/js/data/main.js
@@ -1,2 +1,35 @@
+// Firebase Get Data
+
+// Firebase andrep
 import('/firebase/andrep/home/firebase.min.js');
+
+// Firebase andrep
+import('/firebase/myzuu/home/firebase.min.js');
+
+// Firebase Get Data
+
+
+
+
+// Login Dashbaord
 import('/assets/plugins/firebase/profile/firebase.min.js');
+
+// Mengambil Data View andrep
+import('/data/andrep/home/index.js');
+// Mengambil Data View myzuu
+import('/data/myzuu/home/index.js');
+
+
+
+(() => {
+    const includes = document.getElementsByTagName('include');
+    [].forEach.call(includes, i => {
+        let filePath = i.getAttribute('src');
+        fetch(filePath).then(file => {
+            file.text().then(content => {
+                i.insertAdjacentHTML('afterend', content);
+                i.remove();
+            });
+        });
+    });
+})();
--- firebase/andrep/home/andrep.js
+++ firebase/andrep/home/andrep.js
@@ -11,29 +11,17 @@ var config = {
 
 firebase.initializeApp(config);
 
-// Create a variable to reference the database.
 var database = firebase.database();
-
 var connectionsRef = database.ref("/andrep Home");
 var connectedRef = database.ref(".info/connected");
 
-// When the client's connection state changes...
 connectedRef.on("value", function (snap) {
-
-    // If they are connected..
     if (snap.val()) {
-
-        // Add user to the connections list.
         var con = connectionsRef.push(true);
-        // Remove user from the connection list when they disconnect.
         con.onDisconnect().remove();
     }
 });
 
-// When first loaded or when the connections list changes...
 connectionsRef.on("value", function (snap) {
-
-    // Display the viewer count in the html.
-    // The number of online users is the number of children in the connections list.
     $("#andrep-home").text(snap.numChildren());
 });
